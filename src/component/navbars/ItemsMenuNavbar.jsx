89f96d2906cb97e43f46d2bf0de4841eca3c4441
Alejandro Perez <aperez@velosimo.com>
2023-01-14 23:14:02 -0500

    Changes NavbarMenu

--- src/component/navbars/ItemsMenuNavbar.jsx
+++ src/component/navbars/ItemsMenuNavbar.jsx
@@ -1,6 +1,8 @@
 import { Popover, Transition } from "@headlessui/react";
-import { ArrowDownIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { Fragment } from "react";
+
+import styles from "./navbar.module.scss";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -8,20 +10,20 @@ function classNames(...classes) {
 
 export default function ItemsMenuNavbar({ title, items }) {
   return (
-    <Popover className="relative">
+    <Popover className="relative rounded-none">
       {({ open }) => (
         <>
           <Popover.Button
             className={classNames(
-              open ? "text-red-500" : "text-gray-900",
-              "group inline-flex items-center font-medium hover:text-red-500 "
+              open ? "text-red-500 rounded-none" : "text-gray-900",
+              "group inline-flex items-center font-medium rounded-none  hover:text-red-500 "
             )}
           >
             {title}
             <ChevronDownIcon
               className={classNames(
-                open ? "text-red-500" : "text-gray-900",
-                "ml-2 h-5 w-5 group-hover:text-red-500"
+                open ? "text-red-500 rounded-none" : "text-gray-900",
+                "ml-2 h-5 w-5 rounded-none group-hover:text-red-500"
               )}
               aria-hidden="true"
             />
@@ -29,27 +31,24 @@ export default function ItemsMenuNavbar({ title, items }) {
 
           <Transition
             as={Fragment}
-            enter="transition ease-out duration-200"
+            enter="transition ease-out rounded-none duration-200"
             enterFrom="opacity-0 translate-y-1"
             enterTo="opacity-100 translate-y-0"
             leave="transition ease-in duration-150"
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
-              <div className="overflow-hidden shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+            <Popover.Panel className="absolute rounded-none left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
+              <div className="overflow-hidden rounded-none shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative rounded-none grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                   {items.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
                       className="-m-3 block  p-3 transition duration-150 ease-in-out hover:bg-gray-50"
                     >
-                      <p className="text-base font-medium text-gray-900">
+                      <p className={styles.p}>
                         {item.name}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {item.description}
                       </p>
                     </a>
                   ))}
--- src/component/navbars/NavbarMenuPages.jsx
+++ src/component/navbars/NavbarMenuPages.jsx
@@ -53,14 +53,15 @@ export default function NavbarMenuPages() {
   );
 
   return (
-    <div className=" z-10 h-32 mb-10  flex bg-white">
-      <div class="h-5/6  w-1/4  flex text-center content-center ">
-        <img className="h-20 w-15" src="/img/logo.png" />
+    <div className=" z-10 mb-10 flex  h-32 bg-white">
+      <div class="flex  h-5/6  w-1/4 content-center text-center ">
+        <img src="/img/logo.png" />
       </div>
-      <div class=" h-full w-full">
-        <div className='h-12 bg-red-500 text-right content-center text-white w-full'> 
-        <div className="flex flex-wrap items-center"></div>info@inmocor.com</div>
-        <div className="hidden h-full w-full content-center items-center lg:block">{navList}</div>
+      <div class=" h-full w-full bg-white">
+        <div className={styles.toolbar}>info@inmocor.com</div>
+        <div className=" ml-20 mt-5 flex h-full w-full content-center items-center lg:block">
+          {navList}
+        </div>
       </div>
     </div>
   );

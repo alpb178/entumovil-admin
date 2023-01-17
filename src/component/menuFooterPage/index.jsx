4bf6bc729decee0a0b75d6d39cdcac3fd952defd
Alejandro Perez <aperez@velosimo.com>
2023-01-17 11:16:51 -0500

    Menu Footer

--- src/component/menuFooterPage/index.jsx
+++ src/component/menuFooterPage/index.jsx
@@ -4,6 +4,7 @@ import { Typography } from "@material-tailwind/react";
 import {
   ArrowDownIcon,
   ArrowLongRightIcon,
+  ChevronRightIcon,
   HomeIcon,
 } from "@heroicons/react/24/solid";
 import styles from "./topImage.module.scss";
@@ -18,7 +19,39 @@ export default function MenuFooterPage() {
           <div class="w-1/2  p-2 text-center">
             <img alt="Card Image" src="/img/sticky-logo.png" height={581} />
           </div>
-          <div class=" ml-10 w-1/2  p-2 text-center">Enlaces</div>
+          <div class=" ml-10 w-1/2  p-2 text-center">
+            Enlaces
+            <a className={styles.Icon} href={INMOCOR_HOME_PAGE}>
+              <div className="mt-5 flex content-center">
+                <ChevronRightIcon className="mr-2 h-5 w-5" />
+                ESPAÑA, POTENCIA TURÍSTICA
+              </div>
+            </a>
+            <a className={styles.Icon} href={INMOCOR_HOME_PAGE}>
+              <div className="mt-5 flex content-center">
+                <ChevronRightIcon className="mr-2 h-5 w-5" />
+                BOOM INMOBILIARIO
+              </div>
+            </a>
+            <a className={styles.Icon} href={INMOCOR_HOME_PAGE}>
+              <div className="mt-5 flex content-center">
+                <ChevronRightIcon className="mr-2 h-5 w-5" />
+                BRITÁNICOS COMPRAN MÁS
+              </div>
+            </a>
+            <a className={styles.Icon} href={INMOCOR_HOME_PAGE}>
+              <div className="mt-5 flex content-center">
+                <ChevronRightIcon className="mr-2 h-5 w-5" />
+                INVERSIÓN EN ARGENTINA
+              </div>
+            </a>
+            <a className={styles.Icon} href={INMOCOR_HOME_PAGE}>
+              <div className="mt-5 flex content-center">
+                <ChevronRightIcon className="mr-2 h-5 w-5" />
+                SITUACIÓN INMOBILIARIA
+              </div>
+            </a>
+          </div>
           <div class=" ml-10 w-1/2  p-2 text-center">Enlaces</div>
           <div class=" ml-10 w-1/2  p-2 text-center">Contacto</div>
         </div>

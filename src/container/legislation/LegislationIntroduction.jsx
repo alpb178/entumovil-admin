c2f833a8b7278f34cde9509158c4b186baff73df
Alejandro Perez <aperez@velosimo.com>
2023-02-01 14:50:14 -0500

    update

--- src/container/legislation/LegislationIntroduction.jsx
+++ src/container/legislation/LegislationIntroduction.jsx
@@ -1,65 +1,215 @@
 import FooterSite from "@/component/footer";
 import MenuFooterPage from "@/component/menuFooterPage";
 import TopImage from "@/component/topImage";
-import { INMOCOR_LEGISLATION_AL, INMOCOR_LEGISLATION_AR, INMOCOR_LEGISLATION_CA, INMOCOR_LEGISLATION_ES, INMOCOR_LEGISLATION_EU, INMOCOR_LEGISLATION_FR, INMOCOR_LEGISLATION_IR, INMOCOR_LEGISLATION_IT, INMOCOR_LEGISLATION_RU, INMOCOR_LEGISLATION_UE } from "@/lib/constants";
+import {
+  INMOCOR_LEGISLATION_AL,
+  INMOCOR_LEGISLATION_AR,
+  INMOCOR_LEGISLATION_CA,
+  INMOCOR_LEGISLATION_ES,
+  INMOCOR_LEGISLATION_EU,
+  INMOCOR_LEGISLATION_FR,
+  INMOCOR_LEGISLATION_IR,
+  INMOCOR_LEGISLATION_IT,
+  INMOCOR_LEGISLATION_RU,
+  INMOCOR_LEGISLATION_UE,
+} from "@/lib/constants";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 export default function LegislationIntroduction() {
   return (
     <>
-      <TopImage name="Legislación" description="Normativa de los diferentes ordenamientos jurídicos" imageUrl="cabecera-legislacion-inmocor.jpg" />
-     
-      <section className=" text-xl text-gray bg-legislation-gray h-80" >
+      <TopImage
+        name="Legislación"
+        description="Normativa de los diferentes ordenamientos jurídicos"
+        imageUrl="cabecera-legislacion-inmocor.jpg"
+      />
+
+      <section className=" h-80 bg-legislation-gray text-xl text-gray">
         <div className="container mx-auto">
           <div className="item-center content-center  ">
-          <h1 className="block antialiased tracking-normal font-sans text-5xl font-semibold leading-tight mb-6 text-red">INMOLAW: Un corpus legislativo especializado</h1>
-          <p>El presente apartado recoge la <strong>legislación aplicable a los contratos de compraventa de viviendas</strong> relativa a <strong>España, Argentina, Reino Unido</strong> (con especial referencia a <strong>Inglaterra y Gales</strong>), Irlanda, <strong>Estados Unidos</strong> (concretamente, en el Estado de <strong>Luisiana</strong>), Francia, Italia, Alemania y Cataluña; así como la <strong>normativa comunitaria europea</strong>. La identificación de dicha normativa ha hecho posible la creación de un corpus legislativo, al que hemos denominado <strong>INMOLAW</strong>, un recurso de gran utilidad para responder a las necesidades documentales de profesionales como los traductores jurídicos, agentes inmobiliarios, notarías o juristas, entre otros.
-            Desde los diferentes apartados de esta sección se puede acceder a la normativa de los ordenamientos jurídicos implicados en el estudio, además de a la publicación original donde se recogen los textos jurídicos en cuestión gracias a los hipervínculos presentes en cada texto.
-          </p>
-        </div>
+            <h1 className="mb-6 block font-sans text-5xl font-semibold leading-tight tracking-normal text-red antialiased">
+              INMOLAW: Un corpus legislativo especializado
+            </h1>
+            <p>
+              El presente apartado recoge la{" "}
+              <strong>
+                legislación aplicable a los contratos de compraventa de
+                viviendas
+              </strong>{" "}
+              relativa a <strong>España, Argentina, Reino Unido</strong> (con
+              especial referencia a <strong>Inglaterra y Gales</strong>),
+              Irlanda, <strong>Estados Unidos</strong> (concretamente, en el
+              Estado de <strong>Luisiana</strong>), Francia, Italia, Alemania y
+              Cataluña; así como la{" "}
+              <strong>normativa comunitaria europea</strong>. La identificación
+              de dicha normativa ha hecho posible la creación de un corpus
+              legislativo, al que hemos denominado <strong>INMOLAW</strong>, un
+              recurso de gran utilidad para responder a las necesidades
+              documentales de profesionales como los traductores jurídicos,
+              agentes inmobiliarios, notarías o juristas, entre otros. Desde los
+              diferentes apartados de esta sección se puede acceder a la
+              normativa de los ordenamientos jurídicos implicados en el estudio,
+              además de a la publicación original donde se recogen los textos
+              jurídicos en cuestión gracias a los hipervínculos presentes en
+              cada texto.
+            </p>
+          </div>
         </div>
       </section>
 
-
-      <section className="mt-10 mb-20 flex text-xl text-gray px-4">
+      <section className="mt-10 mb-20 flex px-4 text-xl text-gray">
         <div className="container mx-auto">
           <div className="item-center content-center  ">
-            <div className="row bg-white m-0 px-md-4 py-5">
-              <div className="col-12 mb-4 mb-md-0">
+            <div className="row px-md-4 m-0 bg-white py-5">
+              <div className="col-12 mb-md-0 mb-4">
                 <div className="section-title mb-4">
-                  <h1 className="block antialiased tracking-normal font-sans text-5xl font-semibold leading-tight mb-6 text-black">Legislaciones incluidas</h1>
-                  <p>Seleccione el ámbito legislativo de su interés. También puede utilizar el menú superior para ello.
+                  <h1 className="mb-6 block font-sans text-5xl font-semibold leading-tight tracking-normal text-black antialiased">
+                    Legislaciones incluidas
+                  </h1>
+                  <p>
+                    Seleccione el ámbito legislativo de su interés. También
+                    puede utilizar el menú superior para ello.
                   </p>
                 </div>
                 <ul className="list-check ">
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i>
-                    <h5 className="d-inline"><a href={INMOCOR_LEGISLATION_UE} className="icon-btn btn-link btn-arrow text-dark">Legislación en la Unión Europea<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="flex-inline ml-5 flex ">
+                      <a
+                        href={INMOCOR_LEGISLATION_UE}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en la Unión Europea
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_ES} className="icon-btn btn-link btn-arrow text-dark">Legislación en España<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_ES}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en España<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_AR} className="icon-btn btn-link btn-arrow text-dark">Legislación en Argentina<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_AR}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Argentina<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_RU} className="icon-btn btn-link btn-arrow text-dark">Legislación en Reino Unido<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_RU}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Reino Unido<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_EU} className="icon-btn btn-link btn-arrow text-dark">Legislación en EE. UU.<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_EU}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en EE. UU.<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_IR} className="icon-btn btn-link btn-arrow text-dark">Legislación en Irlanda<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_IR}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Irlanda<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_AL} className="icon-btn btn-link btn-arrow text-dark">Legislación en Alemania<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_AL}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Alemania<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_FR} className="icon-btn btn-link btn-arrow text-dark">Legislación en Francia<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_FR}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Francia<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_IT} className="icon-btn btn-link btn-arrow text-dark">Legislación en Italia<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_IT}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Italia<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                   <li className="border-bottom flex-inline flex pb-3">
-                    <i className="fas fa-check m-1"></i><h5 className="d-inline"><a href={INMOCOR_LEGISLATION_CA} className="icon-btn btn-link btn-arrow text-dark">Legislación en Cataluña<span></span></a></h5>
+                    <div class="rounded-full bg-legislation-gray">
+                      <i className="fas fa-check m-1 rounded-full text-red" />
+                    </div>
+                    <h5 className="ml-5">
+                      <a
+                        href={INMOCOR_LEGISLATION_CA}
+                        className="icon-btn btn-link btn-arrow text-dark flex-inline flex"
+                      >
+                        Legislación en Cataluña<span></span>
+                        <ArrowRightIcon className="mt flex h-7 w-10 ml-2  font-black text-red hover:w-20" />
+                      </a>
+                    </h5>
                   </li>
                 </ul>
               </div>
@@ -67,10 +217,9 @@ export default function LegislationIntroduction() {
           </div>
         </div>
       </section>
-      <section className="px-4 pt-0 text-xl text-gray pb-10">
+      <section className="px-4 pt-0 pb-10 text-xl text-gray">
         <div className="container mx-auto">
-          <div className="item-center flex">
-          </div>
+          <div className="item-center flex"></div>
         </div>
       </section>
       <MenuFooterPage />

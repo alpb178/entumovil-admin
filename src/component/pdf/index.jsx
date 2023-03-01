995b4992c040be60a3ef746d3e09a17befb15f11
Alejandro Perez <aperez@velosimo.com>
2023-03-01 11:36:31 -0500

    Changes find

--- src/component/pdf/index.jsx
+++ src/component/pdf/index.jsx
@@ -1,21 +1,23 @@
 import { Doc } from "@/lib/constants";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import React, { useEffect, useState } from "react";
-import fs from "vite-plugin-fs/browser";
 
 const ViewPDf = ({ radio, valueSelect, findText }) => {
   const [docToShow, setDocToShow] = useState([]);
 
-  const readTxt = (src) => {
-    fs.readFile(`/public/doc/${src.replace("FO", "TXT")}.txt`)
-      .then((response) => {
-        response.includes(findText) && setDocToShow((prev) => [...prev, src]);
-      })
-      .catch({ return: false });
+  const readTxt = async (src) => {
+    await fetch(`/doc/${src.replace("FO", "TXT")}.txt`).then((res) =>
+      res
+        .text()
+        .then(
+          (response) =>
+            response.includes(findText) &&
+            setDocToShow((prev) => [...prev, src])
+        )
+    );
   };
 
   useEffect(() => {
-    console.log("Componente renderizado!!");
     setDocToShow([]);
     Doc.map(({ src }) => {
       if (
@@ -31,8 +33,11 @@ const ViewPDf = ({ radio, valueSelect, findText }) => {
     <>
       {docToShow?.length > 0 ? (
         <div className="mb-10 h-40 overflow-auto shadow-none">
-          {docToShow.map((doc) => (
-            <li key={doc} className="border-bottom flex-inline mb-4 flex pb-3">
+          {docToShow.map((doc, index) => (
+            <li
+              key={index}
+              className="border-bottom flex-inline mb-4 flex pb-3"
+            >
               <div className="rounded-full bg-legislation-gray">
                 <i className="fas fa-check m-1 rounded-full text-red" />
               </div>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "vite-plugin-fs";
 
 export default defineConfig({
-  plugins: [react(), fs()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },

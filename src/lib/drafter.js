5e415cf5e5c797b4fc2e7c1f2eccdb5fb665275d
Alejandro Perez <aperez@velosimo.com>
2023-03-10 16:48:01 -0500

    add changes

---
+++ src/lib/drafter.js
@@ -0,0 +1,64 @@
+export const contractDrafterESP = [
+    {
+      name: "Título",
+      options: [
+        {
+          father: 'title',
+          name: "option 1",
+          value: "CONTRATO DE COMPRAVENTA DE VIVIENDA",
+        },
+        {
+          father: 'title',
+          name: "option 2",
+          value: "CONTRATO DE ALQUILER DE VIVIENDA",
+        },
+      ],
+    },
+    {
+      name: "Lugar",
+      options: [
+        {
+          father: 'sumary',
+          name: "option 1",
+          value: "En XXX, a XXX de XXX del XXX",
+        },
+        {
+          father: 'sumary',
+          name: "option 2",
+          value: "En la ciudad de XXX, a XXX. de XXX. de XXX",
+        },
+      ],
+    },
+    {
+      name: "Partes",
+      options: [
+        {
+          father: 'parts',
+          name: "option 1",
+          value: `De un lado la parte vendedora, D XXX, mayor de edad, con
+          domicilio en XXX, con DNI nº XXX casado en régimen de comunidad de
+          gananciales con Dª.XXX, mayor de edad, con domicilio en XXX, y con
+          DNI nº XXX. Y de otro la parte compradora, DXXX, mayor de edad,
+          con domicilio en XXX, de estado civil soltero y con DNI nº XXX.
+          Ambas partes tienen y se reconocen la capacidad legal necesaria
+          para el otorgamiento del presente contrato , y a tal fin.`
+        },
+        {
+          father: 'parts',
+          name: "option 2",
+          value: `DE UNA PARTE: D. XXX. y Dª XXX, mayores de edad, cónyuges,
+          con domicilio en XXX, calle XXX, nº XXX, y cuyos números de Documento
+          Nacional de Identidad son los XXX. y XXX, respectivamente, en adelante
+          los VENDEDORES;
+          Y DE OTRA PARTE: D. XXX, XXX, mayor de edad, y con domicilio en XXX,
+          calle XXX. nº XXX, provisto del DNI nº XXX, en adelante el COMPRADOR.
+          INTERVIENEN: Todos en su propio nombre y derecho. Ambas partes
+          se reconocen
+          mutuamente la capacidad legal necesaria para el otorgamiento y firma del presente contrato
+          de compraventa, para lo cual,`
+        },
+      ],
+    },
+    
+  ];
+  
